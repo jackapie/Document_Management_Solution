5c98fe84117e1fc92935cd9aa578931ba0e02160
Jackie Pearce <jackiepearce83@hotmail.com>
2019-01-18 14:36:00 +0000

    Added a DocumentId string field to DocumentModel, this is distinct from the Id PrimaryKey field

--- Document Management Solution/Models/DocumentModel.cs
+++ Document Management Solution/Models/DocumentModel.cs
@@ -8,6 +8,7 @@ namespace Document_Management_Solution.Models
     public class DocumentModel
     {
         public int Id { get; set; }
+        public string DocumentId { get; set; }
         public string DocumentTitle { get; set; }
         public byte[] File {get; set;}
     }
