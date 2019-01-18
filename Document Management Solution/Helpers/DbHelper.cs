8e9f5de5029f7acb9c8572f1f5b7662207797a91
Jackie Pearce <jackiepearce83@hotmail.com>
2019-01-18 21:39:16 +0000

    Created AddDocument method in DbHelper

--- Document Management Solution/Helpers/DbHelper.cs
+++ Document Management Solution/Helpers/DbHelper.cs
@@ -32,5 +32,16 @@ namespace Document_Management_Solution.Helpers
             var document = context.DocumentModels.Where((e) => e.DocumentTitle == Title).First();
             return document;
         }
+
+        public void AddDocument(string DocumentTitle, string DocumentId, byte[] File)
+        {
+            var context = new DocManagerContext();
+            var document = new DocumentModel();
+            context.DocumentModels.Add(document);
+            document.DocumentTitle = DocumentTitle;
+            document.DocumentId = DocumentId;
+            document.File = File;
+            context.SaveChanges();
+        }
     }
 }
